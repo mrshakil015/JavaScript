2ed27fcdb450030a3a2d08955b5a4936bf1b84da
Md Shakil <shakil.eub.cse@gmail.com>
2023-10-01 00:32:45 +0600

    Introduction

--- 01- Introduction/01. Introduction.js
+++ 01- Introduction/01. Introduction.js
@@ -1,9 +1,9 @@
 function myFunction() {
-    document.getElementById("demo").innerHTML = "After Click 1- innerHTML() working."
+    document.getElementById("demo").innerHTML = "After Click 1- innerHTML() working.";
 }
 function myFunction2() {
-    document.write("After Click 2- document.write() working.")
+    document.write("After Click 2- document.write() working.");
 }
 function myFunction3() {
-    document.write("After Click 3- window.alert() working.")
+    document.write("After Click 3- window.alert() working.");
 }
