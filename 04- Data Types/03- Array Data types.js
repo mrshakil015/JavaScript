0021a2d365d92ac6c7a0dadf59c88cc41871e469
Md Shakil <shakil.eub.cse@gmail.com>
2023-10-06 23:31:50 +0600

    Data Types

---
+++ 04- Data Types/03- Array Data types.js
@@ -0,0 +1,12 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits);
+console.log("Convert Array to string: "+ fruits.toString());
+console.log("Length of Array: ",fruits.length);
+console.log("Sorts the Array: ",fruits.sort());
+console.log("Accessing the first array element: ",fruits[0]);
+console.log("Accessing the last array element: ",fruits[fruits.length-1]);
+
+fruits.push("Lemon")
+console.log("Adding array element: ",fruits);
+
+
