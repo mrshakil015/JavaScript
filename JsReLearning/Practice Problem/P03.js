537c68a7cc381d799d8da91fe183d2eb9d6c0a77
Md. Shakil <shakil.eub.cse@gmail.com>
2024-08-02 00:09:30 +0600

    updated

---
+++ JsReLearning/Practice Problem/P03.js
@@ -0,0 +1,9 @@
+//Practice Problem -3
+/*
+John's teacher gave him two variables.
+*/
+var val1 = 'I am going to be'
+var val2 = 'an awesome web developer'
+
+var output = val1 + ' ' + val2
+console.log(output)
