95dcb8424ee9901cd92066d5554d16a64362ecff
Md. Shakil <shakil.eub.cse@gmail.com>
2024-08-09 02:23:38 +0600

    Event Handler Method

--- JsReLearning/DOMEvent/js/script.js
+++ JsReLearning/DOMEvent/js/script.js
@@ -18,3 +18,26 @@ const greenButton = document.getElementById('make-green');
 greenButton.onclick = function makeGreen(){
     document.body.style.backgroundColor = 'green';
 }
+
+
+// Option 4:
+
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+// Option 4: Another
+
+const makePurpleButton = document.getElementById('make-purple');
+makePurpleButton.addEventListener('click', function makePurple(){
+ document.body.style.backgroundColor = 'purple';
+})
+
+// Option 4: Final
+//document.getElementById('make-orange').addEventListener('click', function(){})
+document.getElementById('make-orange').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'orange';
+})
