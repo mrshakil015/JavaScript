564e023ed92a1285b8594d7999280650af6654ad
Md. Shakil <shakil.eub.cse@gmail.com>
2024-08-08 12:20:25 +0600

    duplicate

--- JsReLearning/Array/duplicate.js
+++ JsReLearning/Array/duplicate.js
@@ -1,11 +1,11 @@
-const names = ['Rohim','Korim','Jodu','Modu','Rohim','Jodu','Kodu'];
+const names = ["Rohim",'Korim','Jodu','modu','Jodu','Rohim','modu'];
 
 function removeDuplicate(names){
     const unique = [];
     for (let i = 0; i <names.length; i++){
         const name =names[i];
         if(unique.includes(name) == false){
-            unique.push(name)
+            unique.push(name);
         }
     }
     return unique;
