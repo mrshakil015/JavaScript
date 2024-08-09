ac840515b6b21efad4cc24e12e3bf3f0b2960399
Md. Shakil <shakil.eub.cse@gmail.com>
2024-08-10 00:56:08 +0600

    Bank Project Done

--- JsReLearning/Baap-er-bank-project/js/bank.js
+++ JsReLearning/Baap-er-bank-project/js/bank.js
@@ -20,3 +20,29 @@ document.getElementById('deposit-btn').addEventListener('click',function(){
     inputDeposit.value = '';
 
 });
+document.getElementById('withdraw-btn').addEventListener('click',function(){
+    const inputWithdraw = document.getElementById('input-withdraw');
+    const newWithdraw = parseFloat(inputWithdraw.value);
+
+    const totalWithdrawElement = document.getElementById('total-withdraw');
+    const preWithdraw = parseFloat(totalWithdrawElement.innerText);
+
+    const currentWithdrawAmount = preWithdraw + newWithdraw;
+
+    const totalBalanceElement = document.getElementById('total-balance');
+    const preBalanceAmount = parseFloat(totalBalanceElement.innerText);
+    const currentBalance  = preBalanceAmount - newWithdraw;
+
+    if(currentBalance < 0){
+        alert('Not enough blance');
+    }
+    else{
+        
+        totalWithdrawElement.innerText = currentWithdrawAmount;
+        totalBalanceElement.innerText = currentBalance;
+    }
+
+
+    inputWithdraw.value = '';           
+
+});
