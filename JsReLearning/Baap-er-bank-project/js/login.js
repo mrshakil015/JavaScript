4efcfc6f6b1a8619bc188da1eb0a0640d3fc29cb
Md. Shakil <shakil.eub.cse@gmail.com>
2024-08-09 11:47:46 +0600

    Bank Login System

---
+++ JsReLearning/Baap-er-bank-project/js/login.js
@@ -0,0 +1,18 @@
+// Step-1: Add click event handler with the submit button
+document.getElementById("submit-btn").addEventListener("click", function () {
+  // Step-2: get email
+  const emailField = document.getElementById("useremail");
+  const userEmail = emailField.value;
+
+  // Step-3: get password
+  const passwordField = document.getElementById("userpassword");
+  const userPassword = passwordField.value;
+
+  // Step-4: verify email and password
+  if(userEmail=== 'bank@gmail.com' && userPassword === 'bank'){
+    console.log('Valid User');
+  }
+  else{
+    console.log('Invalid User');
+  }
+});
