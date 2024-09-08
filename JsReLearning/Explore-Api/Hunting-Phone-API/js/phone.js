ec8655db02f41f3773d6aec93c6116a2a14b57e6
Md. Shakil <shakil.eub.cse@gmail.com>
2024-09-08 08:15:17 +0600

    search phone completed

--- JsReLearning/Explore-Api/Hunting-Phone-API/js/phone.js
+++ JsReLearning/Explore-Api/Hunting-Phone-API/js/phone.js
@@ -1,5 +1,5 @@
-const loadPhone = async () =>{
-    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+const loadPhone = async (searchText) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data
     // console.log(phones);
@@ -7,12 +7,12 @@ const loadPhone = async () =>{
 
 }
 
-
 const displayPhones = phones =>{
-    console.log(phones);
-
     // 1. Get the id
     const phoneContainer = document.getElementById('phone-container');
+
+    // clear phone container cards before adding new cards
+    phoneContainer.textContent = '';
     phones.forEach(phone => {
         console.log(phone);
         // 2. Create a div
@@ -38,6 +38,14 @@ const displayPhones = phones =>{
         phoneContainer.appendChild(phoneCard);
     });
     
+}
+
+// Handle search button
+const handleSearch = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
 }
 
 
