c84fa01e1a2154bea5f4d9dbb783d48e997bcd64
Md. Shakil <shakil.eub.cse@gmail.com>
2024-09-13 01:12:11 +0600

    Create 7.notation.js

---
+++ JsReLearning/Js-concepts-for-react/7.notation.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'Lal Nil Holud mia',
+    profession: 'Traffic Surgeon',
+    age:39,
+    'son-name': 'Rohim',
+    address: 'Shahabag',
+};
+
+// dot notation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person['profession']
+const pName = 'profession';
+const prof3 = person[pName];
+const son = person['son-name'];
